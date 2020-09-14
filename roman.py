11991b783df563be2fbab78f041652560c153abf
Marcus Autenrieth <myself@prakti.org>
2020-09-14 21:49:16 +0200

    :sparkles: Case 8 to VIII

--- roman.py
+++ roman.py
@@ -3,7 +3,17 @@
 roman_numbers = {"I":1, "V":5, "X":10, }
 
 def to_roman(arabic):
-    if arabic <= 3:
-        return arabic * "I"
-    else:
-        return "IV"
+    roman = ""
+
+    while arabic > 0:
+        if arabic >= 5:
+            roman += "V"
+            arabic -= 5
+        elif arabic == 4:
+            roman += "IV"
+            arabic -= 4
+        elif arabic < 4:
+            roman += arabic * "I"
+            arabic = 0
+
+    return roman
--- test.py
+++ test.py
@@ -12,6 +12,8 @@ class TestRomanNumerals(unittest.TestCase):
     def test_arabic_4_to_roman_IV(self):
         self.assertEqual(roman.to_roman(4), "IV")
 
+    def test_arabic_8_to_roman_VIII(self):
+        self.assertEqual(roman.to_roman(8), "VIII")
 
 
 if __name__ == '__main__':
