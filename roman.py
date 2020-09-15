86449df9f7866406ff674b64591d8f8d67e057e0
Marcus Autenrieth <myself@prakti.org>
2020-09-15 08:36:37 +0200

    :construction: Simplified the whole thing

--- roman.py
+++ roman.py
@@ -1,22 +1,32 @@
 """Libray to convert between roman and arabic numbers"""
 
-roman_numbers = {"I":1, "V":5, "X":10, }
+roman_numerals = [
+    ("M", 1000), ("CM", 900),
+    ("D", 500), ("CD", 400),
+    ("C", 100), ("XC", 90),
+    ("L", 50), ("XL", 40),
+    ("X", 10), ("IX", 9),
+    ("V", 5), ("IV", 4),
+    ("I", 1)
+]
+
+
+def find_next_numeral(arabic):
+    "Finds the next numeral and the corresponding arabic value"
+    for numeral, value in roman_numerals:
+        if arabic >= value:
+            return numeral, value
+    return "", 0
+
 
 def to_roman(arabic):
+    "Converts from arabic numbers to roman numbers. Casts floats to int."
     roman = ""
+    arabic = int(arabic)
 
     while arabic > 0:
-        if arabic == 9:
-            roman += "IX"
-            arabic -= 9
-        elif arabic >= 5:
-            roman += "V"
-            arabic -= 5
-        elif arabic == 4:
-            roman += "IV"
-            arabic -= 4
-        elif arabic < 4:
-            roman += arabic * "I"
-            arabic = 0
+        next_numeral, subtractor = find_next_numeral(arabic)
+        roman += next_numeral
+        arabic -= subtractor
 
     return roman
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import unittest
 import roman
+import math
 
 class TestRomanNumerals(unittest.TestCase):
 
@@ -17,6 +18,30 @@ class TestRomanNumerals(unittest.TestCase):
 
     def test_arabic_9_to_roman_IX(self):
         self.assertEqual(roman.to_roman(9), "IX")
+
+    def test_arabic_13_to_roman_XIII(self):
+        self.assertEqual(roman.to_roman(13), "XIII")
+
+    def test_arabic_47_to_roman_XLVII(self):
+        self.assertEqual(roman.to_roman(47), "XLVII")
+
+    def test_arabic_777_to_roman_DCCLXXVII(self):
+        self.assertEqual(roman.to_roman(777), "DCCLXXVII")
+
+    def test_arabic_1987_to_roman_MCMLXXXVII(self):
+        self.assertEqual(roman.to_roman(1987), "MCMLXXXVII")
+
+    def test_arabic_7949_to_roman_MMMMMMMCMXLIX(self):
+        self.assertEqual(roman.to_roman(7949), "MMMMMMMCMXLIX")
+
+    def test_arabic_minus_to_roman_ERROR(self):
+        self.assertEqual(roman.to_roman(-10), "")
+
+    def test_arabic_zero_to_roman_empty(self):
+        self.assertEqual(roman.to_roman(0), "")
+
+    def test_pi_to_roman_empty(self):
+        self.assertEqual(roman.to_roman(math.pi), "III")
 
 if __name__ == '__main__':
     unittest.main()
